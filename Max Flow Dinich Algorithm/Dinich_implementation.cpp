8d9a213a255bf2dd9d00d6c7a7197165303e72b8
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2018-05-27 02:06:43 +0600

    Dinich Implementation

--- Max Flow Dinich Algorithm/Dinich_implementation.cpp
+++ Max Flow Dinich Algorithm/Dinich_implementation.cpp
@@ -1,85 +1,90 @@
-struct dinic_maxFlow
+#define MX_N 5003
+typedef long long intl;
+const intl inf = 1e12;
+
+struct MaxFlow_Dinich
 {
-        static const int MX_N = 105;
-        int nodes, src, snk, dist[MX_N], start[MX_N];
+    int nodes, src, snk, dist[MX_N], start[MX_N];
+    struct Edge
+    {
+        int nxt, f, cap, rev;
+    };
+    vector<Edge> G[MX_N];
 
-        struct Edge
+    void initialize( int _n )
+    {
+        nodes = _n;
+        for(int i=0; i<=_n; i++)
+            G[i].clear();
+    }
+
+    void addEdge( int u, int v, int w )
+    {
+        Edge p = { v, 0, w, (int)G[v].size() };
+        Edge q = { u, 0, w, (int)G[u].size() };
+        G[u].push_back(p);
+        G[v].push_back(q);
+    }
+
+    bool bfs()
+    {
+        memset(dist, -1, sizeof dist);
+        queue<int> Q;
+        Q.push(src), dist[src] = 0;
+
+        while( !Q.empty() )
         {
-            int nxt, flw, cap, rev;
-        };
-        vector<Edge> G[MX_N];
-
-        void initialize( int _n )
-        {
-            nodes = _n;
-            for(int i=0; i<=nodes; i++) G[i].clear();
-        }
-
-        void addEdge( int u, int v, int w )
-        {
-            Edge p = { v, 0, w, (int)G[v].size() };
-            Edge q = { u, 0, w, (int)G[u].size() };
-            G[u].push_back(p);
-            G[v].push_back(q);
-        }
-
-        bool bfs() //for finding augmenting path
-        {
-            for(int i=0; i<=nodes; i++) dist[i] = -1;
-            queue<int> Q; Q.push( src ), dist[src] = 0;
-
-            while( !Q.empty() )
-            {
-                int u = Q.front(); Q.pop();
-                for(int i=0; i<(int)G[u].size(); i++)
-                {
-                    Edge& e = G[u][i];
-                    int v = e.nxt;
-                    if( dist[v]==-1 && e.flw<e.cap )
-                    {
-                        dist[v] = dist[u] + 1;
-                        Q.push(v);
-                    }
-                }
-            }
-            return ~dist[snk];
-        }
-
-        int dfs( int u, int flow ) //for findig flow 
-        {
-            if( u==snk ) return flow;
-
-            for(int i=start[u], sz=G[u].size(); i<sz; i++)
+            int u = Q.front();
+            Q.pop();
+            for(int i=0; i<(int)G[u].size(); i++)
             {
                 Edge& e = G[u][i];
-                if( e.flw>=e.cap ) continue;
-
                 int v = e.nxt;
-                if( dist[v]==dist[u] + 1 )
+                if( ~dist[v] || e.f>=e.cap )
+                    continue;
+                dist[v] = dist[u]+1;
+                Q.push(v);
+            }
+        }
+        return ~dist[snk];
+    }
+
+    intl dfs( int u, intl flw )
+    {
+        if( u==snk )
+            return flw;
+
+        for(int& i=start[u]; i<(int)G[u].size(); i++)
+        {
+            Edge& e = G[u][i];
+            if( e.f>=e.cap )
+                continue;
+            int v = e.nxt;
+            if( dist[v]==dist[u]+1 )
+            {
+                intl _f = dfs( v, min(flw, (intl)(e.cap-e.f)) );
+                if( _f )
                 {
-                    int f = dfs( v, min(flow, e.cap-e.flw) );
-                    if( f>0 )
-                    {
-                        e.flw += f;
-                        G[v][e.rev].flw -= f;
-                        return f;
-                    }
+                    e.f += _f;
+                    G[v][e.rev].f -= _f;
+                    return _f;
                 }
             }
-            return 0;
         }
+        return 0LL;
+    }
 
-        int maxFlow( int _src, int _snk )
+    intl maxFlow( int _s, int _t )
+    {
+        src = _s, snk = _t;
+        intl ret = 0;
+
+        while( bfs() )
         {
-            src = _src, snk = _snk;
-            int ret = 0;
-
-            while( bfs() )
-            {
-                for(int i=0; i<=nodes; i++) start[i] = 0;
-                while( int tmp = dfs( src, INT_MAX ) )
-                    ret += tmp;
-            }
-            return ret;
+            memset(start, 0, sizeof start);
+            while( intl tmp = dfs( src, inf ) )
+                ret += tmp;
         }
+        return ret;
+    }
 } FLOW;
